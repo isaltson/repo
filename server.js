369ae87cc3bc916eafef9d46ab323e23e8f95755
isaltson <isaltson99@gmail.com>
2025-03-19 17:33:23 +0000

    server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use environment variable for port
 
-// Middleware to parse JSON
+// Middleware to parse JSON and enable CORS
 app.use(express.json());
+app.use(cors());
 
 // Validate reCAPTCHA and password strength
 app.post('/validate', async (req, res) => {
@@ -12,13 +14,14 @@ app.post('/validate', async (req, res) => {
 
   // Validate reCAPTCHA
   try {
-    const reCAPTCHA_URL = `https://www.google.com/recaptcha/api/siteverify?secret=6Le5mvkqAAAAAE4zkFx-i17rB9a6eEVoYxkBOQe3&response=${captchaResponse}`;
+    const reCAPTCHA_URL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captchaResponse}`;
     const reCAPTCHA_Response = await axios.post(reCAPTCHA_URL);
 
     if (!reCAPTCHA_Response.data.success) {
       return res.status(400).json({ error: 'reCAPTCHA validation failed. Please try again.' });
     }
   } catch (error) {
+    console.error('Error validating reCAPTCHA:', error); // Log the error
     return res.status(500).json({ error: 'Error validating reCAPTCHA. Please try again.' });
   }
 
